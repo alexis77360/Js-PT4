c4de9c31ef2107bbde9796bc2049b9d407e04ed0
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-07-25 17:16:14 +0200

    tri pays

--- countrie-app/index.js
+++ countrie-app/index.js
@@ -1,8 +1,10 @@
 //? https://restcountries.com/v3.1/all
 
 const countriesContainer = document.querySelector(".countries-container");
+const btnSort = document.querySelectorAll(".btnSort");
 
 let countriesData = [];
+let sortMethod = "MaxToMin";
 
 
 
@@ -11,8 +13,6 @@ async function fetchCountries() {
     await fetch("https://restcountries.com/v3.1/all").then((res) => res.json()).then((data) => {
 
         countriesData = data;
-
-        console.log(data);
         countriesDisplay();
     });
 
@@ -24,12 +24,29 @@ async function fetchCountries() {
 function countriesDisplay() {
     countriesContainer.innerHTML = countriesData
 
-    .filter((country) =>
-    //? on convertie l'entrée utilisateur en minuscule et on vérifie si elle est inclu dans le nom du pays
-    country.translations.fra.common.toLowerCase().includes(inputSearch.value.toLowerCase()))
-    
-    .map((country) =>
-        `
+        //! Filtrer les pays en fonction de l'entrée utilisateur
+        .filter((country) =>
+            //? on convertie l'entrée utilisateur en minuscule et on vérifie si elle est inclu dans le nom du pays
+            country.translations.fra.common.toLowerCase().includes(inputSearch.value.toLowerCase()))
+
+
+        .sort((a, b) => {
+            if (sortMethod === "maxToMin") {
+                return b.population - a.population;
+            } else if (sortMethod === "minToMax") {
+                return a.population - b.population;
+            } else if (sortMethod === "alpha") {
+                return a.translations.fra.common.localeCompare(b.translations.fra.common);
+            }
+        })
+
+
+        //! Afficher le nombre de pays en fonction du range
+        .slice(0, inputRange.value)
+
+        //! Afficher les pays
+        .map((country) =>
+            `
         <div class="card">
             <img src="${country.flags.svg}" alt="drapeau ${country.translations.fra.common}">
             <h2>${country.translations.fra.common}</h2>
@@ -37,9 +54,8 @@ function countriesDisplay() {
             <p>Population : ${country.population.toLocaleString()}</p>
 
         </div>
-   
-   `
-    ).join("");
+            `
+        ).join("");
 }
 
 //? Charger les pays au chargement de la page
@@ -47,3 +63,17 @@ window.addEventListener("load", fetchCountries());
 
 //? Rejoue countriesDisplay à chaque fois que l'utilisateur tape une lettre
 inputSearch.addEventListener("input", countriesDisplay);
+
+//? Modifie le span à chaque fois que l'utilisateur bouge le range
+inputRange.addEventListener("input", () => {
+    countriesDisplay();
+    rangeValue.innerHTML = inputRange.value;
+});
+
+//? Change la valeur de sortMethod en fonction du bouton cliqué pour le tri
+btnSort.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        sortMethod = e.target.id;
+        countriesDisplay();
+    });
+});
